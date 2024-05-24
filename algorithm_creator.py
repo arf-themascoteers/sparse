dc41cfa62accb8e1369ac6fc6fbe323538bfca0a
arif <arif@themascoteers.com>
2024-05-24 14:02:57 +1000

    done

--- algorithm_creator.py
+++ algorithm_creator.py
@@ -1,16 +1,7 @@
-from algorithms.algorithm_lasso import AlgorithmLasso
-from algorithms.algorithm_spa import AlgorithmSPA
-from algorithms.algorithm_mcuve import AlgorithmMCUVE
-from algorithms.algorithm_pcal import AlgorithmPCALoading
-
 from algorithms.algorithm_bsnet import AlgorithmBSNet
 from algorithms.algorithm_zhang import AlgorithmZhang
 from algorithms.algorithm_bsdr import AlgorithmBSDR
-
-from algorithms.algorithm_reconstructor import AlgorithmReconstructor
-
 from algorithms.algorithm_linspacer import AlgorithmLinspacer
-from algorithms.algorithm_ssparse import AlgorithmSSparse
 
 
 class AlgorithmCreator:
@@ -18,18 +9,10 @@ class AlgorithmCreator:
     def create(name, target_size, splits, repeat, fold, verbose=True):
 
         algorithms = {
-            "lasso" : AlgorithmLasso,
-            "spa" : AlgorithmSPA,
-            "mcuve" : AlgorithmMCUVE,
-            "pcal" : AlgorithmPCALoading,
             "bsnet" : AlgorithmBSNet,
             "zhang" : AlgorithmZhang,
             "bsdr" : AlgorithmBSDR,
-            "rec" : AlgorithmReconstructor,
-
             "linspacer": AlgorithmLinspacer,
-
-            "ssparse" : AlgorithmSSparse
         }
 
         if name not in algorithms:
