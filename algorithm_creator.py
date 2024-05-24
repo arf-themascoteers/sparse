30c1499b1ecc7bac0127f84d549b6f4fe9c0e3b9
arif <arif@themascoteers.com>
2024-05-24 16:51:33 +1000

    done

--- algorithm_creator.py
+++ algorithm_creator.py
@@ -3,6 +3,8 @@ from algorithms.algorithm_zhang import AlgorithmZhang
 from algorithms.algorithm_bsdr import AlgorithmBSDR
 from algorithms.algorithm_bsdr500 import AlgorithmBSDR500
 from algorithms.algorithm_bsdr3000 import AlgorithmBSDR3000
+from algorithms.algorithm_bsdr4000 import AlgorithmBSDR4000
+from algorithms.algorithm_bsdr6000 import AlgorithmBSDR6000
 from algorithms.algorithm_linspacer import AlgorithmLinspacer
 
 
@@ -16,13 +18,15 @@ class AlgorithmCreator:
             "bsdr" : AlgorithmBSDR,
             "bsdr500" : AlgorithmBSDR500,
             "bsdr3000" : AlgorithmBSDR3000,
+            "bsdr4000" : AlgorithmBSDR3000,
+            "bsdr6000" : AlgorithmBSDR6000,
             "linspacer": AlgorithmLinspacer,
         }
 
         if name not in algorithms:
             raise KeyError(f"No algorithm named {name} exists")
 
-        if name in ["bsdr", "bsdr500", "bsdr3000"]:
+        if name in ["bsdr", "bsdr500", "bsdr3000","bsdr4000", "bsdr6000"]:
             return algorithms[name](target_size, splits, repeat, fold, verbose=verbose)
 
         return algorithms[name](target_size, splits)
