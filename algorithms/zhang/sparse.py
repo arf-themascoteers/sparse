9a150b9c62c8b83c797d23f4302265d640c99eb5
arif <arif@themascoteers.com>
2024-05-24 15:04:28 +1000

    done

--- algorithms/zhang/sparse.py
+++ algorithms/zhang/sparse.py
@@ -8,9 +8,8 @@ class Sparse(nn.Module):
         self.criterion = torch.nn.MSELoss(reduction='sum')
 
     def forward(self, X):
-        batch_size = X.shape[0]
-        threshold = 100
-        k = torch.tensor(threshold/batch_size).to(X.device)
+        threshold = 100/128
+        k = torch.tensor(threshold).to(X.device)
         X = torch.where(X < k, 0, X)
         return X
 
