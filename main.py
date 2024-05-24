f48196392935e5fd5ccc270c7f821ffb9a72b7e6
arif <arif@themascoteers.com>
2024-05-24 16:52:29 +1000

    done

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from task_runner import TaskRunner
 
 if __name__ == '__main__':
     tasks = {
-        "algorithms" : ["linspacer","bsdr","bsdr500","bsdr3000"],
+        "algorithms" : ["linspacer","bsdr500","bsdr","bsdr3000","bsdr4000","bsdr6000"],
         "datasets" : ["indian_pines"],
         "target_sizes" : [25, 30, 5, 10, 15, 20]
     }
-    ev = TaskRunner(tasks,1,10,"4.csv",skip_all_bands=True)
+    ev = TaskRunner(tasks,1,10,"5.csv",skip_all_bands=True)
     ev.evaluate()
