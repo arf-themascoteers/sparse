0f1aac594b91c28be885a655946dd7467dd64882
arif <arif@themascoteers.com>
2024-05-25 12:08:34 +1000

    done

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from task_runner import TaskRunner
+import oak
 
 if __name__ == '__main__':
+    result_file = "10.csv"
     tasks = {
         "algorithms" : ["zhangfc","zhang"],
         "datasets" : ["indian_pines"],
         "target_sizes" : [25, 30, 5, 10, 15, 20]
     }
-    ev = TaskRunner(tasks,1,10,"9.csv",skip_all_bands=True)
+    ev = TaskRunner(tasks,1,10,"10.csv",skip_all_bands=True)
     ev.evaluate()
--- oak.py
+++ oak.py
@@ -1,5 +1,7 @@
 import oak_csv_creator
 import oak_plotter
 
-csv = oak_csv_creator.create_csv(filter=["6"])
-oak_plotter.plot_oak(source=csv)
+
+def plot_oak(loc):
+    csv = oak_csv_creator.create_csv(loc)
+    oak_plotter.plot_oak(source=csv)
