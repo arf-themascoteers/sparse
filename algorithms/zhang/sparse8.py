9ab7fd548d0912420b36fe96f1ac20f53216dbf1
Arif <arif@themascoteers.com>
2024-05-30 05:20:13 +1000

    start

---
+++ algorithms/zhang/sparse8.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class Sparse8(nn.Module):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+
+    def forward(self, X):
+        if self.parent.epoch < 100:
+            return X
+        X = torch.where(X < 0.005, 0, X)
+        return X
+
