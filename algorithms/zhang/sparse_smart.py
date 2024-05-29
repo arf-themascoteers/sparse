266df685986bf537aa30af3ba93adb628f8a1b80
Arif <arif@themascoteers.com>
2024-05-30 02:49:40 +1000

    start

--- algorithms/zhang/sparse_smart.py
+++ algorithms/zhang/sparse_smart.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-class SparseOnes(nn.Module):
+class SparseNew(nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, X):
-        return torch.ones_like(X).to(X.device)
-
+        F.relu(X-0.03)
