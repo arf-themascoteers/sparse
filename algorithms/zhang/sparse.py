6c3217d6b96214a918b2fd4cbff80666bb8fbc66
Arif <arif@themascoteers.com>
2024-05-31 14:40:58 +1000

    start

--- algorithms/zhang/sparse.py
+++ algorithms/zhang/sparse.py
@@ -6,7 +6,7 @@ class Sparse(nn.Module):
     def __init__(self):
         super().__init__()
         self.criterion = torch.nn.MSELoss(reduction='sum')
-        self.k = 0.3
+        self.k = 100/128
 
     def forward(self, X):
         X = torch.where(X < self.k, 0, X)
---
+++ main3.py
@@ -0,0 +1,13 @@
+from task_runner import TaskRunner
+import oak_plotter
+
+if __name__ == '__main__':
+    tag = "cnnpar"
+    tasks = {
+        "algorithms" : ["zhang_cnn_par"],
+        "datasets" : ["indian_pines"],
+        "target_sizes" : [5, 10, 15, 20, 25, 30]
+    }
+    ev = TaskRunner(tasks,1,tag,skip_all_bands=True, verbose=True)
+    summary, details = ev.evaluate()
+    #oak_plotter.plot_oak(source=summary)
