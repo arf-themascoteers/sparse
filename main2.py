04ee196be0dd7fb83037511255aa67580fb8e53a
Arif <arif@themascoteers.com>
2024-06-14 04:51:16 +1000

    Big changes

--- main2.py
+++ main2.py
@@ -2,11 +2,11 @@ from task_runner import TaskRunner
 import oak_plotter
 
 if __name__ == '__main__':
-    tag = "X"
+    tag = "lw_relu_custom"
     tasks = {
-        "algorithms" : ["linspacer","random"],
+        "algorithms" : ["lw_relu_custom"],
         "datasets" : ["indian_pines"],
         "target_sizes" : [5, 10, 15, 20, 25, 30]
     }
-    ev = TaskRunner(tasks,10,tag,skip_all_bands=True, verbose=True)
+    ev = TaskRunner(tasks,1,tag,skip_all_bands=True, verbose=True)
     summary, details = ev.evaluate()
