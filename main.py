e28abfd8a0a643e3f8f8e1b7cf41bb462d2b4d32
Arif <arif@themascoteers.com>
2024-06-13 20:48:46 +1000

    Big changes

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import oak_plotter
 if __name__ == '__main__':
     tag = "X"
     tasks = {
-        "algorithms" : ["linspacer","simple"],
+        "algorithms" : ["lw"],
         "datasets" : ["indian_pines"],
-        "target_sizes" : [15, 5, 15, 20, 25, 30]
+        "target_sizes" : [15, 5, 10, 20, 25, 30]
     }
-    ev = TaskRunner(tasks,1,tag,skip_all_bands=True, verbose=True)
+    ev = TaskRunner(tasks,10,tag,skip_all_bands=True, verbose=True)
     summary, details = ev.evaluate()
